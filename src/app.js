27de59c7c7d535f49a3bafe59769c2b62a2d2c7e
Neary <kibishiigaming@gmail.com>
2024-12-09 23:13:41 +0100

    Modif Navigation.

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const App = () => {
     return (
         <Router>
             <div>
-                <Navigation />
                 <div className="main-content">
+                <Navigation />
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
