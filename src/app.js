c48f170e551f85f6e8115a08783e3f2700d8a24d
Neary <kibishiigaming@gmail.com>
2024-12-09 11:56:48 +0100

    creation de routes et pages pour home et about

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import "./app.css"
-import Banner from './components/Banner';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Banner from './components/Banner.jsx';
+import Gallery from './components/Gallery.jsx';
+import Home from './pages/home.js';
+import About from './pages/about.js';
+import './app.css';
 
 function App() {
     return (
-        <div>
-            <Navbar />
-            <Banner title="Chez vous, partout et ailleurs" />
-        </div>
+        <Router>
+            <div>
+                <Navbar />
+                <Banner title="Chez vous, partout et ailleurs" />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+                <Gallery />
+            </div>
+        </Router>
     );
 }
 
