208fdeeb890f23b3bc4eefc83e48088dd33add16
Neary <kibishiigaming@gmail.com>
2024-12-13 22:56:22 +0100

    Responsive home page finit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logoFooter from '../assets/images/logo-footer.png';
+import logoFooter from "../assets/images/logo-footer.png";
 
 const Footer = () => {
         return (
             <div className='footer'>
-                <div className="logo-footer">
                     <img src={logoFooter} alt="Logo Kasa" className="logo-footer" />
+                    <p className="text-footer">© 2020 Kasa. All rights reserved</p>
                     </div>
-            </div>
         );
     };
 
