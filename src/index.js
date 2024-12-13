a640cb32be983d55a3f0aa141aff8c9c218ec653
Neary <kibishiigaming@gmail.com>
2024-12-13 14:34:14 +0100

    responsive tablette et mobile pour navigation et banner ok.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/app.js';
 import reportWebVitals from './reportWebVitals';
+import App from "./app.js"
 import "./styles/index.scss";
 import "./styles/home.scss";
 
