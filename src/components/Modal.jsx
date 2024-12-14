0e504f054014bcee756c896162c8f7867b7593d6
Neary <kibishiigaming@gmail.com>
2024-12-14 01:16:35 +0100

    Responsive house page modale ok.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,48 +1,51 @@
-import React, { useState } from 'react';
-import "../styles/house.scss";
-import Chevrond from "../assets/images/chevron-droit-modal.png";
-import Chevrong from "../assets/images/chevron-gauche-modal.png";
+import React, { useState } from "react";
+import "../styles/house.scss"; // Assurez-vous que ce fichier contient les styles nécessaires
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const Modal = ({ slides }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const goToPrevious = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
     };
 
     const goToNext = () => {
-        const isLastSlide = currentIndex === slides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
     };
 
     return (
-        <div className="modal">
-            <img 
-                src={Chevrong} 
-                alt="Précédent" 
-                onClick={goToPrevious} 
-                className="chevron-left"
-            />
-            <img 
-                src={slides[currentIndex]} 
-                alt={`Slide ${currentIndex + 1}`} 
-                className="modal-image" 
-            />
-            <img 
-                src={Chevrond} 
-                alt="Suivant" 
-                onClick={goToNext} 
-                className="chevron-right"
-            />
-            <div className="slide-counter">
-                {currentIndex + 1} / {slides.length}
-            </div>
+    <div className="modal">
+      {/* Bouton pour aller à la diapositive précédente */}
+        <button className="chevron-left" onClick={goToPrevious} aria-label="Précédent">
+        <FontAwesomeIcon icon={faChevronLeft} size="2x" />
+        </button>
+
+      {/* Image de la diapositive actuelle */}
+        <img
+        src={slides[currentIndex]}
+        alt={`Slide ${currentIndex + 1}`}
+        className="modal-image"
+        />
+
+      {/* Bouton pour aller à la diapositive suivante */}
+        <button className="chevron-right" onClick={goToNext} aria-label="Suivant">
+        <FontAwesomeIcon icon={faChevronRight} size="2x" />
+        </button>
+
+      {/* Compteur des diapositives */}
+        <div className="slide-counter">
+        {currentIndex + 1} / {slides.length}
         </div>
+    </div>
     );
 };
 
 export default Modal;
+
+
 
