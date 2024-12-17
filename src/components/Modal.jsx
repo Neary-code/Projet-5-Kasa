955a397579bda5af3313e3b14e7706db9e20fd32
Neary <kibishiigaming@gmail.com>
2024-12-17 09:37:14 +0100

    Rajout d'une condition pour la modale si il y a une seul photo, pas de compteur ni de chevron.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,41 +7,48 @@ const Modal = ({ slides }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
     };
 
     const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
     };
 
     return (
-    <div className="modal">
-        <button className="chevron-left" onClick={goToPrevious} aria-label="Précédent">
-        <FontAwesomeIcon icon={faChevronLeft} size="2x" />
-        </button>
+        <div className="modal">
+            {slides.length > 1 && (
+                <button className="chevron-left" onClick={goToPrevious} aria-label="Précédent">
+                    <FontAwesomeIcon icon={faChevronLeft} size="2x" />
+                </button>
+            )}
 
-        <img
-        src={slides[currentIndex]}
-        alt={`Slide ${currentIndex + 1}`}
-        className="modal-image"
-        />
+            <img
+                src={slides[currentIndex]}
+                alt={`Slide ${currentIndex + 1}`}
+                className="modal-image"
+            />
 
-        <button className="chevron-right" onClick={goToNext} aria-label="Suivant">
-        <FontAwesomeIcon icon={faChevronRight} size="2x" />
-        </button>
+            {slides.length > 1 && (
+                <button className="chevron-right" onClick={goToNext} aria-label="Suivant">
+                    <FontAwesomeIcon icon={faChevronRight} size="2x" />
+                </button>
+            )}
 
-        <div className="slide-counter">
-        {currentIndex + 1} / {slides.length}
+            {slides.length > 1 && (
+                <div className="slide-counter">
+                    {currentIndex + 1} / {slides.length}
+                </div>
+            )}
         </div>
-    </div>
     );
 };
 
 export default Modal;
+
 
 
 
